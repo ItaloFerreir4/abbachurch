fe60ea5a525be94a8fdae97a52f2de70e589cd67
ItaloFerreir4 <italo.s.ferreira@hotmail.com>
2023-12-07 15:50:41 -0300

    conexao

---
+++ src/backend/config/conexao-bd.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+// Configurações da conexão com o banco de dados
+// const config = {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+// };
+
+// teste
+
+const config = {
+    host: "162.241.3.11",
+    user: "italof39_admin",
+    password: "WFG7wSx)}8nX",
+    database: "italof39_abbachurch",
+};
+
+module.exports = config;
